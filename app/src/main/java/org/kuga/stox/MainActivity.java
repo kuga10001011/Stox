1be4424d72c9a0956ff5f956fee6f4209f31bd94
kruszows <kruszows@u.rochester.edu>
2022-03-20 18:37:41 -0400

    trading screen base

--- app/src/main/java/org/kuga/stox/MainActivity.java
+++ app/src/main/java/org/kuga/stox/MainActivity.java
@@ -2,7 +2,10 @@ package org.kuga.stox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startTrading(View view) {
+        Intent intent = new Intent(this, TradingScreen.class);
+        EditText inputAPIKeyBox = findViewById(R.id.input_api_key_edit_text);
+        EditText outputAPIKeyBox = findViewById(R.id.output_api_key_edit_text);
+        String inputAPIKey = inputAPIKeyBox.getText().toString();
+        String outputAPIKey = outputAPIKeyBox.getText().toString();
+        intent.putExtra("INPUT_API_KEY", inputAPIKey);
+        intent.putExtra("OUTPUT_API_KEY", outputAPIKey);
+        if (inputAPIKey.length() > 0 && outputAPIKey.length() > 0) {
+            startActivity(intent);
+        }
+        else {
+            //open popup window to handle invalid keys
+        }
     }
 }
---
+++ app/src/main/java/org/kuga/stox/TradingScreen.java
@@ -0,0 +1,15 @@
+package org.kuga.stox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TradingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trading_screen);
+    }
+
+}
